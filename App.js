10543a4dc53f68bf4d6e3ff97472fa4dc4887721
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-03-27 11:03:40 -0300

    Styles and final adjusts
    Here we final our final things, now we also have an dark mode, to give the user a fun expercience and we also have the final layout and organization of everything.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import { ActivityIndicator, Text, Provider, DarkTheme, DefaultTheme } from 'react-native-paper';
+import { StyleSheet, View, Animated, Dimensions, TouchableOpacity } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Text, View, Animated, Dimensions } from 'react-native';
 import ToastManager, { Toast } from 'toastify-react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Fontisto, Ionicons } from '@expo/vector-icons';
@@ -22,8 +23,14 @@ export default function App() {
 
   const scrollX = new Animated.Value(0);
 
-  const width = Dimensions.get('window').width;
   const height = Dimensions.get('window').height;
+  const width = Dimensions.get('window').width;
+
+  const [loading, setLoading] = React.useState(true);
+
+  const [mesure, setMesure] = React.useState('C');
+
+  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
 
   // FONTS
   const [loaded] = useFonts({
@@ -31,6 +38,35 @@ export default function App() {
     'Nunito-Bold': require('./assets/fonts/Nunito/static/Nunito-Bold.ttf'),
     'Nunito-Black': require('./assets/fonts/Nunito/static/Nunito-Black.ttf'),
   });
+
+  // THEME
+  const CustomDefaultTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#fff',
+      text: '#01497c',
+      title: '#012a4a',
+      card: '#012a4a',
+      cardText: '#fff',
+      icons: '#01497c',
+    }
+  }
+
+  const CustomDarkTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme,
+      background: '#012a4a',
+      text: '#cfcfcf',
+      title: '#fff',
+      card: '#fff',
+      cardText: '#012a4a',
+      icons: '#cfcfcf',
+    }
+  }
+
+  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
 
   // GET WEATHER
   const getWeather = async () => {
@@ -40,6 +76,9 @@ export default function App() {
       })
       .catch((error) => {
         Toast.error(error.response.data.message);
+      })
+      .finally(() => {
+        setLoading(false);
       })
   }
 
@@ -68,6 +107,10 @@ export default function App() {
     })();
   }
 
+  function switchTheme() {
+    setIsDarkTheme(!isDarkTheme);
+  }
+
   // USE EFFECT
   React.useEffect(() => {
     if (location) {
@@ -81,80 +124,105 @@ export default function App() {
     return null;
   }
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container} mode="padding">
-        <ToastManager />
-        <StatusBar style="auto" />
-        <View style={styles.header}>
-          <Text style={[styles.titleHeader, { fontFamily: 'Nunito-Regular' }]}>{weather?.location.name}, {weather?.location.region}</Text>
-          <Text style={[styles.subtitleHeader, { fontFamily: 'Nunito-Regular' }]}>{weather?.location.country}</Text>
-        </View>
-        <View style={[styles.main, { width: width, height: height / 2 }]}>
-          <Text style={[styles.subtitleMain, { fontFamily: 'Nunito-Regular' }]}>{moment().format('LT')}</Text>
-          <Text style={[styles.titleMain, { fontFamily: 'Nunito-Bold' }]}>{parseInt(weather?.current.temp_c)}°C</Text>
-          <Text style={[styles.subtitleMain, { fontFamily: 'Nunito-Regular' }]}>{weather?.current.condition.text}</Text>
-          <View style={styles.subInfo}>
-            <View style={{flexDirection: 'row', marginHorizontal: 10, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Ionicons name="ios-water-outline" size={22} color="#01497c" />
-            <Text style={[styles.label, { fontFamily: 'Nunito-Regular' }]}>{parseInt(weather?.current.humidity)}%</Text>
-            </View>
-            <View style={{flexDirection: 'row', marginHorizontal: 10, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Fontisto name="wind" size={22} color="#01497c" />
-            <Text style={[styles.label, { fontFamily: 'Nunito-Regular' }]}>{parseInt(weather?.current.wind_kph)}m/s</Text>
-            </View>
-            <View style={{flexDirection: 'row', marginHorizontal: 10, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Fontisto name="day-sunny" size={22} color="#01497c" />
-            <Text style={[styles.label, { fontFamily: 'Nunito-Regular' }]}>{parseInt(weather?.current.uv)}</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ width: width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={[{ fontFamily: 'Nunito-Black', color: '#012a4a', textTransform: 'uppercase', fontSize: RFValue(15), marginVertical: 5 }]}>Temperaturas do dia</Text>
-        <Animated.ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          onScroll={() => {
-            Animated.event(
-              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-              { useNativeDriver: false })
-          }}
-          scrollEventThrottle={30}
-        >
+    <Provider theme={theme}>
+      <SafeAreaProvider>
+        <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]}>
+          <ToastManager />
+          <StatusBar style="auto" />
           {
-            data?.map((item, index) => {
-              return (
-                <View key={index} style={[styles.block, { width: width / 5.5, height: 90}]}>
-                  <Text style={[styles.subtitleBlock, { fontFamily: 'Nunito-Regular' }]}>{moment(item.time).format('LT')}</Text>
-                  <Text style={[styles.titleBlock, { fontFamily: 'Nunito-Bold' }]}>{parseInt(item.temp_c)}°C</Text>
-                  <Text style={[styles.subtitleBlock, { fontFamily: 'Nunito-Regular' }]}>{item.condition.text}</Text>
+            loading
+              ?
+              <ActivityIndicator size="large" color={theme.colors.primary} />
+              :
+              <>
+                <View style={styles.header}>
+                  <TouchableOpacity onPress={() => mesure === 'C' ? setMesure('F') : setMesure('C')}>
+                    <Text style={[styles.titleHeader, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{mesure === 'C' ? '°C' : '°F'}</Text>
+                  </TouchableOpacity>
+                  <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Text style={[styles.titleHeader, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{weather?.location.name}, {weather?.location.region}</Text>
+                    <Text style={[styles.subtitleHeader, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{weather?.location.country}</Text>
+                  </View>
+                  <TouchableOpacity onPress={() => switchTheme()}>
+                    <View>
+                      {
+                        isDarkTheme
+                          ?
+                          <Ionicons name="ios-sunny-outline" size={22} color={theme.colors.icons} />
+                          :
+                          <Ionicons name="ios-moon-outline" size={22} color={theme.colors.icons} />
+                      }
+                    </View>
+                  </TouchableOpacity>
                 </View>
-              )
-            })
-          }
-        </Animated.ScrollView>
-        </View>
-        <View style={styles.footer}>
-          {
-            forecast?.forecast.forecastday.map((item, index) => {
-              return (
-                <View key={index} style={styles.item}>
-                  <Text style={[styles.itemText, { fontFamily: 'Nunito-Regular', width: '60%' }]}>{moment(item.date).format('dddd')}, {moment(item.date).format('DD MMMM')}</Text>
-                  <Text style={[styles.itemText, { fontFamily: 'Nunito-Regular', width: '20%', textAlign: 'center' }]}>{parseInt(item.day.maxtemp_c)}°C</Text>
-                  <Text style={[styles.itemText, { fontFamily: 'Nunito-Regular', width: '20%', textAlign: 'center' }]}>{parseInt(item.day.mintemp_c)}°C</Text>
+                <View style={[styles.main, { width: width, height: height / 2 }]}>
+                  <Text style={[styles.subtitleMain, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{moment().format('LT')}</Text>
+                  <Text style={[styles.titleMain, { fontFamily: 'Nunito-Bold', color: theme.colors.title }]}>{mesure === 'C' ? parseInt(weather?.current.temp_c) : parseInt(weather?.current.temp_f)}{mesure === 'C' ? '°C' : '°F'}</Text>
+                  <Text style={[styles.subtitleMain, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{weather?.current.condition.text}</Text>
+                  <View style={styles.subInfo}>
+                    <View style={{ flexDirection: 'row', marginHorizontal: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <Ionicons name="ios-water-outline" size={22} color={theme.colors.icons} />
+                      <Text style={[styles.label, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{parseInt(weather?.current.humidity)}%</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', marginHorizontal: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <Fontisto name="wind" size={22} color={theme.colors.icons} />
+                      <Text style={[styles.label, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{parseInt(weather?.current.wind_kph)}m/s</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', marginHorizontal: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <Fontisto name="day-sunny" size={22} color={theme.colors.icons} />
+                      <Text style={[styles.label, { fontFamily: 'Nunito-Regular', color: theme.colors.text }]}>{parseInt(weather?.current.uv)}</Text>
+                    </View>
+                  </View>
                 </View>
-              )
-            })
+                <View style={{ width: width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <Text style={[{ fontFamily: 'Nunito-Black', color: theme.colors.title, textTransform: 'uppercase', fontSize: RFValue(15), marginVertical: 5 }]}>Temperaturas do dia</Text>
+                  <Animated.ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    onScroll={() => {
+                      Animated.event(
+                        [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                        { useNativeDriver: false })
+                    }}
+                    scrollEventThrottle={30}
+                  >
+                    {
+                      data?.map((item, index) => {
+                        return (
+                          <View key={index} style={[styles.block, { width: width / 5.5, height: 90, backgroundColor: theme.colors.card }]}>
+                            <Text style={[styles.subtitleBlock, { fontFamily: 'Nunito-Regular', color: theme.colors.cardText }]}>{moment(item.time).format('LT')}</Text>
+                            <Text style={[styles.titleBlock, { fontFamily: 'Nunito-Bold', color: theme.colors.cardText }]}>{parseInt(item.temp_c)}°C</Text>
+                            <Text style={[styles.subtitleBlock, { fontFamily: 'Nunito-Regular', color: theme.colors.cardText }]}>{item.condition.text}</Text>
+                          </View>
+                        )
+                      })
+                    }
+                  </Animated.ScrollView>
+                </View>
+                <View style={styles.footer}>
+                  {
+                    forecast?.forecast.forecastday.map((item, index) => {
+                      return (
+                        <View key={index} style={styles.item}>
+                          <Text style={[styles.itemText, { fontFamily: 'Nunito-Regular', color: theme.colors.text, width: '60%' }]}>{moment(item.date).format('dddd')}, {moment(item.date).format('DD MMMM')}</Text>
+                          <Text style={[styles.itemText, { fontFamily: 'Nunito-Regular', color: theme.colors.text, width: '20%', textAlign: 'center' }]}>{parseInt(item.day.maxtemp_c)}°C</Text>
+                          <Text style={[styles.itemText, { fontFamily: 'Nunito-Regular', color: theme.colors.text, width: '20%', textAlign: 'center' }]}>{parseInt(item.day.mintemp_c)}°C</Text>
+                        </View>
+                      )
+                    })
+                  }
+                </View>
+              </>
           }
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     paddingVertical: 20,
     width: '100%',
@@ -162,14 +230,14 @@ const styles = StyleSheet.create({
   },
   header: {
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    width: '100%',
   },
   titleHeader: {
-    color: '#01497c',
     fontSize: RFValue(14),
   },
   subtitleHeader: {
-    color: '#01497c',
     fontSize: RFValue(12),
   },
   main: {
@@ -177,11 +245,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   titleMain: {
-    color: '#012a4a',
     fontSize: RFValue(100),
   },
   subtitleMain: {
-    color: '#01497c',
     fontSize: RFValue(14),
   },
   subInfo: {
@@ -191,7 +257,6 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
   label: {
-    color: '#01497c',
     fontSize: RFValue(15),
     marginLeft: 5,
   },
@@ -209,7 +274,6 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   itemText: {
-    color: '#012a4a',
     fontSize: RFValue(12),
   },
   block: {
@@ -217,14 +281,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginHorizontal: 10,
     borderRadius: 10,
-    backgroundColor: '#012a4a',
   },
   titleBlock: {
-    color: '#fff',
     fontSize: RFValue(18),
   },
   subtitleBlock: {
-    color: '#fff',
     fontSize: RFValue(8),
     textAlign: 'center',
   },
